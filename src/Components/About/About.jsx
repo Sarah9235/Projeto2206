dafd660ef0894f81fd468d79aef628c6d4557850
sarah9235 <sarinha.barcaro11@gmail.com>
2024-06-22 16:34:31 -0300

    Inserindo atualização do code 16:34 - 22/06

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,18 @@
+import './About.css';
+
+export default function About() {
+    return (
+        <div id="about">
+            <div id="aboutImg"></div>
+            <div id="aboutText">
+                <h4 id="aboutHeading">Sua Jornada Começa Aqui!</h4><br />
+                <h1>Porque comprar um pacote de viagem?</h1>
+                <p>Optar por um pacote de viagens traz inúmeras vantagens que podem transformar sua experiência de viagem em algo ainda mais especial e sem preocupações.
+
+</p>
+                <br />
+                <p> Ao escolher um de nossos pacotes na [Nome da Agência de Viagens], você desfruta de conveniência e comodidade, com tudo organizado em um só lugar. Passagens aéreas, hospedagem, traslados, passeios e alimentação são cuidadosamente integrados, evitando as complicações de reservas individuais e simplificando seu planejamento de viagem. Nossa equipe cuida de todos os detalhes, desde o embarque até o retorno, permitindo que você relaxe e aproveite cada momento sem preocupações.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,16 @@
+import './Slider.css'
+
+// SLIDER PÁGINA PRINCIPAL
+export default function Slider() {
+    return (
+        <div id="slider">
+            <div id="slidertext">
+                <h3>Descubra o Mundo com a Gente</h3>
+                <br />
+                <h1>TRANSFORMANDO SONHOS EM DESTINOS!</h1><br />
+                <p>Com uma vasta seleção de destinos ao redor do mundo, oferecemos opções para todos os perfis de viajantes. Quer você esteja procurando uma escapada romântica, uma aventura emocionante, ou uma viagem relaxante em família, temos o pacote perfeito para você.</p><br />
+            </div>
+              
+        </div>
+    )
+}
